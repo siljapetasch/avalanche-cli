4d623c1162d837ca4cde20643e28fbd130506eaf
Felipe Madero <felipe.madero@gmail.com>
2024-03-06 23:35:32 -0300

    add cli command to sh for service preparation

--- pkg/ssh/shell/setRelayerService.sh
+++ pkg/ssh/shell/setRelayerService.sh
@@ -1,2 +1,3 @@
-sudo cp $HOME/.avalanche-cli/services/awm-relayer/awm-relayer.service /etc/systemd/system/awm-relayer.service
+~/bin/avalanche teleporter relayer prepareService
+sudo cp ~/.avalanche-cli/services/awm-relayer/awm-relayer.service /etc/systemd/system/awm-relayer.service
 sudo systemctl daemon-reload
